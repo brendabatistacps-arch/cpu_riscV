6e656b9482136f432414fbc48c8ecdb0e3d98b63
brendabatistacps-arch <brendabatista.cps@gmail.com>
2026-06-16 11:42:49 -0300

    Create splitter entity with data outputs
    Define a splitter entity with two output ports for data.

--- spliter.vhd
+++ spliter.vhd
@@ -1,1 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+entity splitter is
+	port(
+		data_in		:	in std_logic_vector(31 downto 0); -- entrada de dados
+		data_out1	:	out std_logic_vector(15 downto 0); -- saída de dados 1 (data_out1)
+		data_out2	:	out std_logic_vector(15 downto 0) -- saída de dados 2 (data_out2)
+	);
+end splitter;
+
+architecture behavioral of splitter is
+	begin
+		data_out1 <= data_in(31 downto 16);
+		data_out2 <= data_in(15 downto 0);
+end behavioral;
